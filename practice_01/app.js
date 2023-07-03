b49db6539f8e62d1c16effaee0e80b25c169d2c5
A.Sydorka <anton.sydorka@gmail.com>
2023-07-03 09:48:06 +0300

    practice 01 start

---
+++ practice_01/app.js
@@ -0,0 +1,22 @@
+`use strict`
+
+const countOfFilms = +prompt('Count of film', '');
+
+const personalMovieDB = {
+    count: countOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privete: false
+};
+
+let lastFilmTitle = prompt('What last film you see?', '');
+let lastFilmRate = prompt('please rate this film', '');
+
+let lastFilmTitle2 = prompt('What last film you see? (another)', '');
+let lastFilmRate2 = prompt('please rate this film', '');
+
+personalMovieDB.movies[lastFilmTitle] = lastFilmRate;
+personalMovieDB.movies[lastFilmTitle2] = lastFilmRate2;
+
+console.log(personalMovieDB);
